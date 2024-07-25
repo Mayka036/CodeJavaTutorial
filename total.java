6843b8be9fbb32d8e8d71d656d0c03b1ea4304e1
Mayka036 <s66309010036@kktech.ac.th>
2024-07-25 00:32:04 -0700

    Create total.java

---
+++ total.java
@@ -0,0 +1,8 @@
+public class total {
+    public static void main(String[] args) {
+        int number1 = 62;
+        int number2 = 99;
+        int sum = number1 + number2;
+        System.out.println(" " + number1 + " + " + number2 + " = " + sum);
+    }
+}
