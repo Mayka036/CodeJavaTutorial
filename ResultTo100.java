ec348fc58ce5022362b4e42e34a55544fc0d4534
Mayka036 <s66309010036@kktech.ac.th>
2024-07-31 20:05:37 -0700

    Create ResultTo100.java

---
+++ ResultTo100.java
@@ -0,0 +1,11 @@
+public class ResultTo100 {
+    public static void main(String[] args) {
+        int sum = 0,number = 1;
+        while (number <=100) {
+            sum += number;
+            number++;
+        }
+        System.out.println("sum 1 to 100 = "+ sum);
+        }
+    }
+
