173783cf02737bcb39ca8faaa92a5beb446c67ea
Mayka036 <s66309010036@kktech.ac.th>
2024-07-31 22:16:08 -0700

    Create Testdowhile.java

---
+++ Testdowhile.java
@@ -0,0 +1,22 @@
+public class Testdowhile {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i <= 10; i++){
+            System.out.println("i in for : "+i);  
+        }
+        int iWhile = 1;
+        while (iWhile <= 10) {
+            System.out.println("iwhile in while: "+iWhile);
+            iWhile++;
+        }
+        int counter = 1;
+        do{
+            System.out.println("counter in: "+ counter);
+            counter++;
+        }while(counter <=10);
+                
+            
+            
+        }
+    }
+
