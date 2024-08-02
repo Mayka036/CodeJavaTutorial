e9e1c3e2081083c86805b3b0fc45199fc546e3bb
Mayka036 <s66309010036@kktech.ac.th>
2024-08-02 00:59:29 -0700

    Create DemoArg.java

---
+++ DemoArg.java
@@ -0,0 +1,21 @@
+public class DemoArg {
+    public static void main(String[] args) {
+        System.out.println("star in main");
+        int testnum = 10;
+        int total;
+        displayValue(5);
+        displayValue(testnum);
+        displayValue(testnum * 5);
+        total = showSum(3,4);
+        System.out.println("Total have value: "+ total);
+        
+    }
+    public static void displayValue(int num) {
+        System.out.println("Value is"+ num);
+    }
+    public static int showSum(int x,int y) {
+        int sum;
+        sum = x + y;
+        return sum;
+    }
+}
