d7801054f2acecc38d7b6b6d3239644d6192df06
Mayka036 <s66309010036@kktech.ac.th>
2024-08-02 00:59:38 -0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,15 @@
+public class LocalVars {
+        public static void main(String[] args) {
+            phuket();
+            rayoung();
+            phuket();
+        }
+        public static void phuket (){
+            int birds = 5000;
+            System.out.println("In phuket Threre are "+ birds +"birds.");
+        }
+        public static void rayoung (){
+            int birds = 3500;
+            System.out.println("In rayoung Threre are "+ birds +"birds.");
+        }
+}
