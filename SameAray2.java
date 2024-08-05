781aeb7347c0841a89c5aa28f197cc60a7fa4095
Mayka036 <s66309010036@kktech.ac.th>
2024-08-04 21:29:30 -0700

    Update SameAray2.java

--- SameAray2.java
+++ SameAray2.java
@@ -1,6 +1,6 @@
 public class SameAray2 {
     public static void main(String[] args) {
-        int[ ] array1 = [5,4,3,2,1,];
+        int[ ] array1 = {5,4,3,2,1,};
         int[ ] array2 = new int [array1.length];
         for(int i = 0; i < array1.length; i++){
             array2 [i] = array1[i];
@@ -12,9 +12,10 @@ public class SameAray2 {
         for(int i = 0; i < array1.length; i++){
             System.out.println(array1[i]+ "");
         }
-        System.out.println("array1");
-        for(int i = 0; i < array1.length; i++){
-            System.out.println(array1[i]+ "");
+        System.out.println();
+        System.out.println("array2");
+        for(int i = 0; i < array2.length; i++){
+            System.out.println(array2[i]+ "");
         }
     }
 }
